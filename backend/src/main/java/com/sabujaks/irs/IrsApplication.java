d3360ec4eca8c521fe57b8d6eeb53433e0cc3c7f
mpqm <okqkrwhdtjd@gmail.com>
2024-09-03 10:24:08 +0900

    [Init] be, fe 프로젝트 생성

---
+++ backend/src/main/java/com/sabujaks/irs/IrsApplication.java
@@ -0,0 +1,13 @@
+package com.sabujaks.irs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IrsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sabujaks/irs/IrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sabujaks.irs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
