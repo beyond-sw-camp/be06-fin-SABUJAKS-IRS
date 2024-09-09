5a7d3f2ad72024b79e7f42fb5883a05d74cf2920
SihyunSeo <tjtlgus4@naver.com>
2024-09-09 15:59:57 +0900

    [Feat] 면접 일정 등록 기능 개발

--- backend/src/main/java/com/sabujaks/irs/domain/interview_schedule/controller/InterviewScheduleController.java
+++ backend/src/main/java/com/sabujaks/irs/domain/interview_schedule/controller/InterviewScheduleController.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/interview-schedule")
+@RequestMapping("/api/interview-schedule")
 @RequiredArgsConstructor
 public class InterviewScheduleController {
 
