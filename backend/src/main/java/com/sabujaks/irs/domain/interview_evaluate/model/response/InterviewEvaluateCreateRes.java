9422290b8c0b0e96e7a029996dc820ec55d82eb7
mpqm <okqkrwhdtjd@gmail.com>
2024-09-23 00:54:57 +0900

    [Feat] 면접관 지원자 화상 면접 평가 생성 개발 완료

---
+++ backend/src/main/java/com/sabujaks/irs/domain/interview_evaluate/model/response/InterviewEvaluateCreateRes.java
@@ -0,0 +1,12 @@
+package com.sabujaks.irs.domain.interview_evaluate.model.response;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class InterviewEvaluateCreateRes {
+    private Long idx;
+}
--- backend/src/main/java/com/sabujaks/irs/domain/interview_schedule/model/entity/InterviewParticipate.java
+++ backend/src/main/java/com/sabujaks/irs/domain/interview_schedule/model/entity/InterviewParticipate.java
@@ -2,11 +2,14 @@ package com.sabujaks.irs.domain.interview_schedule.model.entity;
 
 import com.sabujaks.irs.domain.auth.model.entity.Estimator;
 import com.sabujaks.irs.domain.auth.model.entity.Seeker;
+import com.sabujaks.irs.domain.interview_evaluate.model.entity.InterviewEvaluate;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Entity
 @Getter
@@ -17,6 +20,9 @@ public class InterviewParticipate {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idx;
+
+    @OneToMany(mappedBy = "interviewParticipate")
+    private List<InterviewEvaluate> interviewEvaluateList;
 
     @ManyToOne
     @JoinColumn(name="interviewSchedule_idx")
