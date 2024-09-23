5fd164a6d3b9922e09338b5545144b81d7fb3431
seo-jae-eun <wodms5522@naver.com>
2024-09-23 20:56:31 +0900

    [Feat] 채용 담당자 서류 합불 개발 완료

---
+++ backend/src/main/java/com/sabujaks/irs/domain/resume/model/request/ResumeUpdateDocPassedReq.java
@@ -0,0 +1,12 @@
+package com.sabujaks.irs.domain.resume.model.request;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ResumeUpdateDocPassedReq {
+    private Boolean docPassed;
+}
