902872bc2800d88d85c1f883464c15fbcc4b6ff2
seo-jae-eun <wodms5522@naver.com>
2024-10-07 14:56:33 +0900

    [Refactor] 채용담당자 지원자 목록 페이징 처리

--- backend/src/main/java/com/sabujaks/irs/domain/resume/repository/ResumeRepository.java
+++ backend/src/main/java/com/sabujaks/irs/domain/resume/repository/ResumeRepository.java
@@ -1,6 +1,8 @@
 package com.sabujaks.irs.domain.resume.repository;
 
 import com.sabujaks.irs.domain.resume.model.entity.Resume;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -20,6 +22,6 @@ public interface ResumeRepository extends JpaRepository<Resume, Long> {
     @Query("SELECT COUNT(r) FROM Resume r WHERE r.announcement.idx = :announcementIdx")
     Long countResumesByAnnouncementIdx(Long announcementIdx);
 
-    @Query("SELECT r FROM Resume r WHERE r.announcement.idx = :announcementIdx")
-    List<Resume> findAllByAnnouncementIdx(Long announcementIdx);
+    @Query("SELECT r FROM Resume r WHERE r.announcement.idx = :announcementIdx ORDER BY r.idx DESC")
+    Page<Resume> findAllByAnnouncementIdx(Long announcementIdx, Pageable pageable);
 }
