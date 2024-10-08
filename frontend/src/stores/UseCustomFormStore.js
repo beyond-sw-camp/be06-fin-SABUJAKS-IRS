4aefa6549167157d455a9ad90f7e88758ff0124c
eunjoo koo <eunjoo.nine@gmail.com>
2024-10-08 12:31:47 +0900

    [Refactor] 채용담당자와 지원자 공고 관련 페이지 개선

--- frontend/src/stores/UseCustomFormStore.js
+++ frontend/src/stores/UseCustomFormStore.js
@@ -1,6 +1,7 @@
 import { defineStore } from 'pinia';
 import axios from 'axios';
 import { backend } from '@/const';
+import { useToast } from 'vue-toastification';
 
 export const UseCustomFormStore = defineStore('customform', {
     state: () => ({
@@ -8,7 +9,7 @@ export const UseCustomFormStore = defineStore('customform', {
     }),
     actions: {
         // 지원서 폼 저장 로직
-        async saveForm(announcementIdx, selectedForms, coverLetterSections) {
+        async saveForm(announcementIdx, selectedForms, coverLetterSections, router) {
             try {
                 const titleList = coverLetterSections.map((section) => section.title);
                 const chatLimitList = coverLetterSections.map((section) => section.characterLimit);
@@ -34,14 +35,22 @@ export const UseCustomFormStore = defineStore('customform', {
                 console.log("응답" + response.data.result);
 
                 if (response.status === 200) {
-                    alert('폼 저장이 완료되었습니다. 공고 메뉴로 이동해 주세요.');
+                    const toast = useToast();
+                    toast.success('폼 저장이 완료되었습니다. 공고 메뉴로 이동합니다.');
 
                     // router를 통해 특정 경로로 리다이렉트
                     // router.push('/recruiter/mypage');
+
+                    // 알림이 끝난 후 이동 (2초 후)
+                    setTimeout(() => {
+                        router.push('/recruiter/announce');
+                    }, 2000); // 2000ms = 2초 (알림 지속 시간에 맞추어 조정)
                 }
             } catch (error) {
                 console.error('폼 저장 실패:', error);
-                alert('폼 저장에 실패하였습니다. ' + error);
+
+                const toast = useToast();
+                toast.error('폼 저장에 실패하였습니다. ' + error.response.data.message);
             }
 
         }
